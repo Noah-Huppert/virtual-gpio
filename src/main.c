2e45f3d7076a746b2fe26f693df61a2389a1bfd0
Noah Huppert <contact@noahh.io>
2018-12-23 03:38:08 -0500

    added max port num command line arg

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #include "./vgpio.h"
 
@@ -19,7 +20,7 @@ int main(int argc, char **argv) {
 			   "\n"
 		       "USAGE\n"
 		       "\n"
-		       "    virtual-gpio CONTROL_FILE_DIR\n"
+		       "    virtual-gpio CONTROL_FILE_DIR [MAX_PORT_NUM]\n"
 			   "\n"
 			   "OPTIONS\n"
 			   "\n"
@@ -28,15 +29,41 @@ int main(int argc, char **argv) {
 		       "ARGUMENTS\n"
 		       "\n"
 		       "    CONTROL_FILE_DIR    Directory to place virtual GPIO\n"
-		"                        control files\n");
+			   "                        control files\n"
+			   "\n"
+			   "    MAX_PORT_NUM        Maximum allowed port number, \n"
+			   "                        optional, defaults to 99\n");
 		exit(0);
 	}
 
 	const char* control_f_dir = argv[1];
+	long int max_port_num = 99;
 
-	vgpio = vgpio_init(control_f_dir);
+	// .... If max port num arg provided
+	if (argc > 2) {
+		char *end_pntr;
+
+		max_port_num = strtol(argv[2], &end_pntr, 10);
+
+		if (argv[2] == end_pntr) { // No number in string
+			fprintf(stderr, "error parsing MAX_PORT_NUM argument into "
+					"integer: \"%s\"\n", argv[2]);
+			exit(1);
+		}
+	}
+
+	if (max_port_num < 0) {
+		fprintf(stderr, "MAX_PORT_NUM argument cannot be negative\n");
+		exit(1);
+	}
+
+	// Initialize virtual GPIO
+	vgpio = vgpio_init(control_f_dir, max_port_num);
 
 	atexit(cleanup);
+
+	// Run virtual GPIO
+	vgpio_run(vgpio);
 
 	return 0;
 }
